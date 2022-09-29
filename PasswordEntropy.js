bc837cb4544b73cf51a73fc46bd26b38b54d94f8
Ziga <ziga.zajc007@gmail.com>
2022-09-29 18:32:39 +0200

    first commit

---
+++ PasswordEntropy.js
@@ -0,0 +1,28 @@
+class PasswordEntropy{
+
+	static lcase = "abcdefghijklmnopqrstuvwxyz";
+	static ucase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	static numb = "1234567890";
+	static symbol = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ";
+
+	static includesChar(text, charlist){
+		for(let i = 0; i < text.length; i++){
+			if(charlist.includes(text[i])) return true;
+		}
+		return false;
+	}
+
+	static calculate(password){
+		if(typeof(password) != 'string' || password == null) password = "";
+
+		let pool = 0;
+		if(this.includesChar(password, this.lcase)) pool += 26;
+		if(this.includesChar(password, this.ucase)) pool += 26;
+		if(this.includesChar(password, this.numb)) pool += 10;
+		if(this.includesChar(password, this.symbol)) pool += 33;
+		if(!this.includesChar(password, this.lcase + this.ucase + this.numb + this.symbol)) pool += 100;
+		if(pool == 0) pool++;
+
+		return Math.round(password.length * Math.log(pool) / Math.LN2);
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# PasswordEntropy-JS
